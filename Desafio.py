59fb56b1c94c40875919e99c1e6100b2479e79e1
Joyce Tenório <joyce.tenorio@ctec.ufal.br>
2019-01-09 10:27:03 -0300

    Inserindo o Desafio

---
+++ Desafio.py
@@ -0,0 +1,8 @@
+frase=input('Digite a frase a ser cifrada:')
+lista=['a', 'b', 'c', 'd']
+for x in frase:
+    print(lista.index(x)+2)
+cifra=input('Digite o número de elementos da cifra:')
+for x in cifra:
+    
+    print(lista(x))
